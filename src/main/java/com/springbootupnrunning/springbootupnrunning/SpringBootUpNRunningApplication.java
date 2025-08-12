5194deaa727175b0825d089c850345bdaf205893
Shin Jihoon <tobeabestns@naver.com>
2025-08-12 18:35:07 +0900

    Initial commit

---
+++ src/main/java/com/springbootupnrunning/springbootupnrunning/SpringBootUpNRunningApplication.java
@@ -0,0 +1,13 @@
+package com.springbootupnrunning.springbootupnrunning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUpNRunningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUpNRunningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootupnrunning/springbootupnrunning/SpringBootUpNRunningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootupnrunning.springbootupnrunning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUpNRunningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
